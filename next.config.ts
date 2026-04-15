a19cfed4c1317da1a2777237fde39cf2132e9960
decodersifat <user.sifathossain@gmail.com>
2026-04-15 16:51:01 +0600

    Config: Allow all external HTTPS image sources in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,28 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "ibass-collection.s3.ap-south-1.amazonaws.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "img.freepik.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "example.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "storage.example.com",
-        pathname: "/**",
+        hostname: "**",
       },
     ],
   },
