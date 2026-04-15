4ce298851e350309c5c484cc151f9b127a1eee64
Shek Rasel <swe.rasel@gmail.com>
2026-04-15 17:15:41 +0600

    config update,

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        // hostname: "**",
         hostname: "ibass-collection.s3.ap-south-1.amazonaws.com",
         pathname: "/",
       },
