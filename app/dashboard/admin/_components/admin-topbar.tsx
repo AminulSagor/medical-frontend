3c995a1ad083a26612751f02a1d1af45fd44fc85
Shek Rasel <swe.rasel@gmail.com>
2026-04-15 20:09:29 +0600

    changes

--- app/dashboard/admin/_components/admin-topbar.tsx
+++ app/dashboard/admin/_components/admin-topbar.tsx
@@ -14,6 +14,7 @@ import type {
   DropdownNotificationItem,
   GetDropdownNotificationsResponse,
 } from "@/types/admin/notifications.types";
+import Link from "next/link";
 
 type AdminTopbarProps = {
   onMenuClick?: () => void;
@@ -136,13 +137,21 @@ export default function AdminTopbar({ onMenuClick }: AdminTopbarProps) {
           </div>
 
           <div className="flex items-center gap-2 sm:gap-3">
-            <div className="hidden items-center gap-2 rounded-md bg-slate-100 px-3 py-2 text-slate-500 md:flex">
+            {/* <div className="hidden items-center gap-2 rounded-md bg-slate-100 px-3 py-2 text-slate-500 md:flex">
               <Search size={16} className="text-slate-400" />
               <input
                 className="w-[220px] bg-transparent text-sm text-slate-600 outline-none placeholder:text-slate-400 lg:w-[260px]"
                 placeholder="Search students, orders..."
               />
-            </div>
+            </div> */}
+            <Link href="/dashboard/admin/users">
+              <div className="hidden items-center gap-2 rounded-md bg-slate-100 px-3 py-2 text-slate-500 md:flex">
+                <Search size={16} className="text-slate-400" />
+                <div className="w-[220px] text-sm text-slate-600 lg:w-[260px]">
+                  Search Users...
+                </div>
+              </div>
+            </Link>
 
             <div className="relative" ref={notifWrapRef}>
               <button
@@ -181,14 +190,15 @@ export default function AdminTopbar({ onMenuClick }: AdminTopbarProps) {
               )}
             </div>
 
-            <button
+            <Link
+              href="/public/home"
               type="button"
               className="inline-flex h-9 items-center gap-2 rounded-md bg-[var(--primary)] px-3 text-xs font-semibold text-white transition hover:bg-[var(--primary-hover)] sm:px-4"
             >
               <span className="hidden sm:inline">Visit Live Site</span>
               <span className="sm:hidden">Visit</span>
               <ExternalLink size={16} strokeWidth={2} />
-            </button>
+            </Link>
           </div>
         </div>
       </div>
